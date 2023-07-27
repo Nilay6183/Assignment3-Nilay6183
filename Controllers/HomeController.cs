5f6659c8c2d276d1dbef1d98e73459296dc1d6a8
Nilay6183 <200533034@student.georgianc.on.ca>
2023-07-27 15:31:21 -0400

    Assignment-3

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using week8_asp.net.Models;
+
+namespace week8_asp.net.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    //s[Authorize(Roles = "Admin")]
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
